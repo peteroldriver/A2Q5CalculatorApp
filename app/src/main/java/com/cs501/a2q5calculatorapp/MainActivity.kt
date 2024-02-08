1fd163f03cc7db04ff29cf6c92a4fbe2bdaf1a8a
peteroldriver <70061894+peteroldriver@users.noreply.github.com>
2024-02-07 22:41:52 -0500

    Final Version Support Simple Operation

--- app/src/main/java/com/cs501/a2q5calculatorapp/MainActivity.kt
+++ app/src/main/java/com/cs501/a2q5calculatorapp/MainActivity.kt
@@ -3,13 +3,12 @@ package com.cs501.a2q5calculatorapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
-import javax.xml.xpath.XPathExpression
-import kotlin.properties.Delegates
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
     //init view component
@@ -35,8 +34,10 @@ class MainActivity : AppCompatActivity() {
 
     var history = 0.0
     var operator = ""
+    var number = 0.0
     var firstFlag = true
     var pointFlag = true
+    var afterAnswer = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,51 +64,96 @@ class MainActivity : AppCompatActivity() {
         b_min = findViewById(R.id.button_min)
 
         b0.setOnClickListener {
+            if(afterAnswer){
+                expression.text.clear()
+                afterAnswer = false
+            }
             checkZero()
             expression.append("0")
         }
 
         b1.setOnClickListener {
+            if(afterAnswer){
+                expression.text.clear()
+                afterAnswer = false
+
+            }
             checkZero()
             expression.append("1")
         }
 
         b2.setOnClickListener {
+            if(afterAnswer){
+                expression.text.clear()
+                afterAnswer = false
+
+            }
             checkZero()
             expression.append("2")
         }
 
         b3.setOnClickListener {
+            if(afterAnswer){
+                expression.text.clear()
+                afterAnswer = false
+
+            }
             checkZero()
             expression.append("3")
         }
 
         b4.setOnClickListener {
+            if(afterAnswer){
+                expression.text.clear()
+                afterAnswer = false
+
+            }
             checkZero()
             expression.append("4")
         }
 
         b5.setOnClickListener {
+            if(afterAnswer){
+                expression.text.clear()
+                afterAnswer = false
+
+            }
             checkZero()
             expression.append("5")
         }
 
         b6.setOnClickListener {
+            if(afterAnswer){
+                expression.text.clear()
+                afterAnswer = false
+            }
             checkZero()
             expression.append("6")
         }
 
         b7.setOnClickListener {
+            if(afterAnswer){
+                expression.text.clear()
+                afterAnswer = false
+            }
             checkZero()
             expression.append("7")
         }
 
         b8.setOnClickListener {
+            if(afterAnswer){
+                expression.text.clear()
+                afterAnswer = false
+            }
             checkZero()
             expression.append("8")
         }
 
         b9.setOnClickListener {
+            if(afterAnswer){
+                expression.text.clear()
+                afterAnswer = false
+            }
             checkZero()
             expression.append("9")
         }
@@ -127,31 +173,189 @@ class MainActivity : AppCompatActivity() {
 
         b_mul.setOnClickListener {
 
-            if(firstFlag){
+            if(!chechOper()){
+                Snackbar.make(it, "Only support simple operation!Pls start over again", Snackbar.LENGTH_LONG)
+                expression.text.clear()
+                history = 0.0
+                number = 0.0
+                operator = ""
+                afterAnswer = true
+                expression.text.clear()
+                return@setOnClickListener
+            }
+            try {
+
                 history = expression.text.toString().toDouble()
             }
-            else{
-                history = eval(history, expression.text.toString().toDouble(), operator)
+            catch (e:Exception){
+                Snackbar.make(it,"Does not read in last number", Snackbar.LENGTH_LONG).show()
+                history = 0.0
+                number = 0.0
+                operator = ""
+                afterAnswer = true
+                expression.text.clear()
+                return@setOnClickListener
+
             }
             operator = "*"
+            expression.text.clear()
+
+        }
+
+        b_plus.setOnClickListener {
+            if(!chechOper()){
+                Snackbar.make(it, "Only support simple operation!Pls start over again", Snackbar.LENGTH_LONG).show()
+                expression.text.clear()
+                history = 0.0
+                number = 0.0
+                operator = ""
+                afterAnswer = true
+                expression.text.clear()
+                return@setOnClickListener
+            }
+            try {
+
+                history = expression.text.toString().toDouble()
+            }
+            catch (e:Exception){
+                Snackbar.make(it,"Does not read in last number", Snackbar.LENGTH_LONG).show()
+                history = 0.0
+                number = 0.0
+                operator = ""
+                afterAnswer = true
+                expression.text.clear()
+                return@setOnClickListener
+
+            }
+            operator = "+"
+            expression.text.clear()
+
+        }
+
+        b_min.setOnClickListener {
+            if(!chechOper()){
+                Snackbar.make(it, "Only support simple operation!Pls start over again", Snackbar.LENGTH_LONG).show()
+                expression.text.clear()
+                history = 0.0
+                number = 0.0
+                operator = ""
+                afterAnswer = true
+                expression.text.clear()
+                return@setOnClickListener
+            }
+            try {
+
+                history = expression.text.toString().toDouble()
+            }
+            catch (e:Exception){
+                Snackbar.make(it,"Does not read in last number", Snackbar.LENGTH_LONG).show()
+                history = 0.0
+                number = 0.0
+                operator = ""
+                afterAnswer = true
+                expression.text.clear()
+                return@setOnClickListener
+
+            }
+            operator = "-"
+            expression.text.clear()
+
+        }
+
+        b_div.setOnClickListener {
+            if(!chechOper()){
+                Snackbar.make(it, "Only support simple operation!Pls start over again", Snackbar.LENGTH_LONG).show()
+                expression.text.clear()
+                history = 0.0
+                number = 0.0
+                operator = ""
+                afterAnswer = true
+                expression.text.clear()
+                return@setOnClickListener
+            }
+            try {
+
+                history = expression.text.toString().toDouble()
+            }
+            catch (e:Exception){
+                Snackbar.make(it,"Does not read in last number", Snackbar.LENGTH_LONG).show()
+                history = 0.0
+                number = 0.0
+                operator = ""
+                afterAnswer = true
+                expression.text.clear()
+                return@setOnClickListener
+
+            }
+            operator = "/"
+            expression.text.clear()
+        }
+
+        b_sqrt.setOnClickListener {
+            Snackbar.make(it, "Type number you want to operate after press sqrt", Snackbar.LENGTH_LONG).show()
+            if(!chechOper()){
+                Snackbar.make(it, "Only support simple operation! Pls start over again", Snackbar.LENGTH_LONG).show()
+                expression.text.clear()
+                history = 0.0
+                number = 0.0
+                operator = ""
+                afterAnswer = true
+                expression.text.clear()
+                return@setOnClickListener
+            }
+            operator = "sqrt"
         }
 
         b_eql.setOnClickListener {
+            number = expression.text.toString().toDouble()
             expression.text.clear()
-            expression.append(history.toString())
+            if(number.equals(0.0) && operator == "/"){
+                Snackbar.make(it, "Cannot divide by Zero!", Snackbar.LENGTH_LONG)
+                return@setOnClickListener
+            }
+            history = eval(history, number, operator);
+            history = (history * 100.0).roundToInt() / 100.0
+            Log.d("TAG", "Answer his: $history")
+            var sign = ""
+            if (history < 0){
+                sign = "-"
+            }
+            expression.append(sign+history.toString())
+            history = 0.0
+            number = 0.0
+            operator = ""
+            afterAnswer = true
         }
 
 
 
     }
 
+    private fun chechOper(): Boolean {
+        if(operator == ""){
+            return true
+        }
+        return false
+    }
+
     fun checkZero(){
-        if(expression.text.equals("0.0") || expression.text.toString().toDouble().equals(0.0)){
+        if(expression.text.toString() == "" || expression.text.toString().toDouble().equals(0.0)){
             expression.text.clear();
         }
     }
 
-    fun eval(history: Double, number:Double, operator:String):Double{
-        
+    private fun eval(history: Double, number: Double, operator: String): Double {
+
+        var res = when (operator) {
+            "+" -> history + number
+            "sqrt" -> sqrt(number)
+            "-" -> history - number
+            "*" -> history * number
+            "/" -> history / number
+            "" -> history
+            else -> 0.0
+        }
+        Log.d("TAG", "Answer : $res")
+        return res
     }
 }
